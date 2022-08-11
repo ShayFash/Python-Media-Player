ce501516b71c0634ffe8f8c284a975ecddf0f6ee
ShayFash <90285583+ShayFash@users.noreply.github.com>
2022-08-11 13:05:15 -0600

    Window Media Ui Update
    Added Window Class, began Media UI

--- py_video_player.py
+++ py_video_player.py
@@ -6,3 +6,22 @@ from PyQt5.QtWidgets import *
 from PyQt5.QtMultimedia import *
 from PyQt5.QtMultimediaWidgets import *
 
+class Window(QMainWindow):
+    def __init__(self, parent=None):
+        super(Window, self).__init__(parent)
+        self.resize(860, 600)
+        self.setWindowTitle('Python Video Player')
+        self.setWindowIcon(QIcon("player.png"))
+        self.ui_init()
+
+    # Media Player UI
+    def ui_init(self):
+        # Menubar objects
+        menu = self.menuBar()
+        file = menu.addMenu("File")
+        file.addAction("Open File")
+        file.triggered[QAction].connect(self.open_file)
+
+
+
+
