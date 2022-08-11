a05cd7eda66ebe31b2fb1023dd7aee45dc70af6c
ShayFash <90285583+ShayFash@users.noreply.github.com>
2022-08-11 12:51:35 -0600

    Create py_video_player.py
    Redoing the code, now that I understand how it works. want to make the code cleaner, and add features

---
+++ py_video_player.py
@@ -0,0 +1,8 @@
+import sys
+import os
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+from PyQt5.QtMultimedia import *
+from PyQt5.QtMultimediaWidgets import *
+
